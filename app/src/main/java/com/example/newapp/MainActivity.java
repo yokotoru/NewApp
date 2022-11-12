d6b1f7bf16f82563d69bba9bf086181266082898
Yoko _ toru <yoko_toru@outlook.com>
2022-11-12 15:20:51 +0300

    new

--- app/src/main/java/com/example/newapp/MainActivity.java
+++ app/src/main/java/com/example/newapp/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.newapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.log_in);
+    }
+
+    public void registClick(View view) {
+        setContentView(R.layout.register);
+    }
+
+    public void logClick(View view) {
+        setContentView(R.layout.log_in);
+    }
+
+    public void getStart(View view) {
+        setContentView(R.layout.getstart);
     }
 }
